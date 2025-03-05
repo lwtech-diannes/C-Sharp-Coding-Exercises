f1554474c1ac75263003bfa95d8e8a6e2020a142
Dianne Scott <79290999+lwtech-diannes@users.noreply.github.com>
2025-03-05 12:18:56 -0800

    Email validation exercise complete (#2)

--- Ex_Files_C_Sharp_Code_Challenges/Exercise Files/01-02-validating-emails/starter/Program.cs
+++ Ex_Files_C_Sharp_Code_Challenges/Exercise Files/01-02-validating-emails/starter/Program.cs
@@ -16,9 +16,32 @@ namespace validatingemails
         }
 
         // MARK: Write your solution here
+        /* Requirements: 
+            - Must contain an '@' symbol
+            - Must end in .com
+            - 1st character cannot be a number or a symbol
+            - Display success message if email is valid
+            - Display error message if email is invalid
+            - Display error message if email is valid
+        */
         public static void ValidateEmail(string email)
         {
-
+            if (char.IsDigit(email[0]) || char.IsSymbol(email[0]))
+            {
+                Console.WriteLine("Email is invalid. First character cannot be a number or symbol.");
+            }
+            else if (!email.Contains("@"))
+            {
+                Console.WriteLine("Email is valid. Please include the '@' symbol.");
+            }
+            else if (!email.EndsWith(".com")) 
+            {
+                Console.WriteLine("Email is valid. Your email must end in '.com'.");
+            }
+            else
+            {
+                Console.WriteLine("Email is valid.");
+            }
         }
     }
 }
