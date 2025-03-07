a1e14f69062b187a94a5c708213086421a8afd6b
lwtech-diannes <diannegermaine@gmail.com>
2025-03-06 20:48:38 -0800

    Completed party RSVP exercise

--- Ex_Files_C_Sharp_Code_Challenges/Exercise Files/01-09-party-rsvp/starter/Program.cs
+++ Ex_Files_C_Sharp_Code_Challenges/Exercise Files/01-09-party-rsvp/starter/Program.cs
@@ -23,7 +23,38 @@ namespace partyrsvp
         // MARK: Write your solution here
         public static void InviteDetails()
         {
+            HashSet<string> allInvitees = new HashSet<string>(family);
 
+            // Combine family and friends. HashSets cannot have duplicates, this will automatically remove them.
+            allInvitees.UnionWith(friends);
+
+            // Print out the total number of invites.
+            Console.WriteLine($"You sent out {allInvitees.Count} invites!\n");
+
+            Console.WriteLine($"Duplicates were sent to:");
+            HashSet<string> duplicates = new HashSet<string>(family);
+
+            // Find duplicates. IntersectWith will only keep the elements that are in both sets.
+            duplicates.IntersectWith(friends);
+            PrintSets(duplicates);
+
+            Console.WriteLine("People who have not sent an RSVP:");
+            // Finds elements that are present in the present object (everyone) or the specified (rsvp) object, but not in both.
+            allInvitees.SymmetricExceptWith(rsvp);
+            PrintSets(allInvitees);
+        }
+
+        // Handy helper to print out the data
+        public static void PrintSets(HashSet<string> names)
+        {
+            int num = 1;
+            foreach (string name in names)
+            {
+                Console.WriteLine($"  {num}. {name}");
+                num++;
+            }
+
+            Console.WriteLine();
         }
     }
 }
